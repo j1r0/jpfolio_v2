a776c73f0049972f5a50b0af75346a0c532db87f
Jim Pamplona <48pamplona@gmail.com>
2025-08-28 12:27:46 -0400

    navbar fixes

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -62,7 +62,7 @@ export const Navbar = ({
 
         }}
         transition={{
-          duration: 0.2,
+          duration: 0.4, ease: "easeInOut", 
         }}
         className=
           {`tablet:flex fixed inset-x-0 max-w-[22rem] mx-auto tablet:mt-0 tablet:max-w-none tablet:w-full z-[5000] items-center justify-center tablet:border-none tablet:rounded-none tablet:p-0 rounded-full
@@ -70,29 +70,28 @@ export const Navbar = ({
             ${scrolled && "backdrop-blur-md tablet:dark:border-white-100/[0.3] tablet:border-black/[0.3] drop-shadow-lg border-black-100/[0.5] dark:border-white-100/[0.5] border-[.5px] mt-4 "}`}
     >
     <motion.nav
-      className={`flex flex-row gap-1 tablet:gap-10 items-center ${
-      scrolled
-        ? "justify-around tablet:justify-between"
-        : "justify-around tablet:justify-end"
-      } tablet:right-0 relative tablet:mx-9 mx-4 lowercase tablet:w-screen py-4`}
+      className={`flex flex-row gap-1 tablet:gap-10 items-center justify-around tablet:justify-end tablet:right-0 relative tablet:mx-9 mx-4 lowercase tablet:w-screen py-4`}
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 1 }}
     >
-      {scrolled ? (
-      <motion.div
-        className={`hidden tablet:flex relative text-center text-xl tablet_lg:text-left tablet:text-2xl desktop:text-4xl uppercase font-light tracking-wider cursor-pointer`}
-        initial={{ opacity: 0, x: -20 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -20 }}
-        transition={{ duration: 0.7, delay: 1 }}
-      >
-        <Link href="/" className="flex flex-row items-center w-10">
-          <img src="jp_logo.svg" alt="JP"/>
-        </Link>
-      </motion.div>
-      ) : null}
+      <AnimatePresence>
+        {scrolled && (
+          <motion.div
+            className={`hidden tablet:flex absolute left-0 text-center text-xl tablet_lg:text-left tablet:text-2xl desktop:text-4xl uppercase font-light tracking-wider cursor-pointer`}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.7, delay: scrolled ? 0.2 : 0.1}}
+          >
+            <Link href="/" className="flex flex-row items-center w-10">
+              <img src="jp_logo.svg" alt="JP"/>
+            </Link>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      
       <div className="flex flex-row gap-2 tablet:gap-10 items-center tablet:justify-between">
       <ThemeSwitcher />
       {navItems.map((item) => (

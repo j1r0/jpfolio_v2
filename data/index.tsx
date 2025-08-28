de6535594f94399857aa11997dd2227abfbbaa8a
Jim Pamplona <48pamplona@gmail.com>
2025-08-28 12:36:43 -0400

    navbar fixes

--- data/index.tsx
+++ data/index.tsx
@@ -32,9 +32,9 @@ import {
 
 export const navItems = [
   { name: "Home", link: "/" },
-  { name: "About", link: "/about" },
-  { name: "Projects", link: "/projects" },
-  { name: "Contact", link: "/contact" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
 ];
 
 export const heroData = {
